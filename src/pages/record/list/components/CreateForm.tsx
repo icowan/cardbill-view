e630c3b0c45c84c96cfd4ae85d96b9cc88e2a2e4
王聪 <solacowa@gmail.com>
2019-11-05 18:29:32 +0800

    清加记录增加信用卡尾号

--- src/pages/record/list/components/CreateForm.tsx
+++ src/pages/record/list/components/CreateForm.tsx
@@ -35,7 +35,7 @@ const CreateForm: React.FC<CreateFormProps> = props => {
   for (let i in creditCards) {
     creditCardList.push(
       <Option key={`card-${creditCards[i].id}`} value={creditCards[i].id}>
-        {creditCards[i].bank.bank_name}-{creditCards[i].card_name}
+        {creditCards[i].bank.bank_name}-{creditCards[i].card_name}-{creditCards[i].tail_number}
       </Option>,
     );
   }
